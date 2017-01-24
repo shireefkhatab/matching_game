3891ce88e7ac489cf6ffa109769fbadfff74046c
shireefkhatab <sherif_19@hotmail.co.uk>
2017-01-24 15:34:17 +0000

    fix click on safari

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ var theLeftSide = document.getElementById("leftSide");
 var theRightSide = document.getElementById("rightSide");
 var theBody = document.getElementsByTagName("body")[0];
 
+var startDiv = document.getElementById('start');
+var startButton = document.getElementById('startGame');
+startButton.addEventListener('click', generateImages);
+
+var replay = document.getElementById('gameOver');
 //generate faces
 function generateImages() {
+
+  startDiv.style.display = 'none';
 	// loop for the number of faces times
   for (var i = 0; i < numberOfImages; i++) {
-	  
+
     var img = document.createElement('img');
+
     var x_position = Math.floor(Math.random() * parseInt(window.getComputedStyle(theLeftSide,null).getPropertyValue("width"))*.8);
     var y_position = Math.floor(Math.random() * parseInt(window.getComputedStyle(theLeftSide,null).getPropertyValue("height"))*.9);
     // image src and position
@@ -28,29 +36,44 @@ function generateImages() {
   rightSideImages = theRightSide.appendChild(theLeftSideImagesClone);
   
   // loop through the leftside images:
-  theLeftSide.childNodes.forEach(function(elem){
-	// if you clicked a wrong image call function gameOver
-	if(elem !=theLeftSide.lastChild){
-		elem.onclick = gameOver;
-	}
-  })
+
+    var array = Array.prototype.slice.call(theLeftSide.childNodes);
+
+    [].forEach.call(theLeftSide.childNodes, function(child) {
+        // if you clicked a wrong image call function gameOver
+        if(child !=theLeftSide.lastChild){
+            child.addEventListener('click', badClick);
+            child.addEventListener('touchstart', badClick);
+        }
+    });
+
   // if you clicked the right image call function nextLevel
-  theLeftSide.lastChild.onclick = nextLevel;
- 
+  theLeftSide.lastChild.addEventListener('click', nextLevel)
+  theLeftSide.lastChild.addEventListener('touchstart', nextLevel);
 };
 
-// game over
-function gameOver() {
-  var replay = document.getElementById('gameOver')
+// bad click
+function badClick() {
   replay.style.display = 'block';
   document.getElementById('finalScore').innerHTML += score;
-  theBody.onclick = null;
-  theLeftSide.lastChild.onclick = null;
+	while (theLeftSide.firstChild) {
+		theLeftSide.removeChild(theLeftSide.lastChild);
+	}
+	while (theRightSide.firstChild) {
+		theRightSide.removeChild(theRightSide.lastChild);
+	}
+  numberOfImages = 2;
+  score = 0;
+
 };
 
 // reload page
-function reload (){
-  window.location.reload(true);
+document.getElementById('reload').onclick = function (){
+    replay.style.display = 'none';
+    startDiv.style.display = 'block';
+    generateImages();
+
+  // window.location.reload(true);
 }
 
 // next level
